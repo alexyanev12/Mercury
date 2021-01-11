a3b7917aca9310ac03318dafc2b0a3d2e32f2c4d
ttangelov18 <59692613+TeodorAngelov@users.noreply.github.com>
2021-01-12 00:07:45 +0200

    code has been shortened and bugs have been fixed
    reduced repeating lines of code by making them functions, fixed another repeating digits related issue

--- Battleship Game/Battleship Game/Battleship Game.cpp
+++ Battleship Game/Battleship Game/Battleship Game.cpp
@@ -2,23 +2,45 @@
 #include <time.h>
 using namespace std;
 
-int guess(int* battleship)
+int* playerInput(int* coord) //used for entering ship coordinates
 {
-	int guess[4], positions = 0, numbers = 0, tries = 14;
-	for (int i = 1; i <= 13; i++) //the 13 tries the player gets
+	for (int i = 0; i < 4; i++)
 	{
-		cout << "Guess the ship's coordinates 4 digits (must be from 0 to 7)" << endl;
-		for (int j = 0; j < 4; j++) //the 4 digits the player guesses
+		cin >> coord[i];
+		if (cin.fail() or coord[i] < 0 or coord[i] > 7)
 		{
-			cin >> guess[j];
-			if (cin.fail() or guess[j] < 0 or guess[j] > 7) //if the input is incorrect
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Oops! One or more numbers you have entered are not in range?! Maybe try something between 0 and 7?" << endl;
+			i--;
+		}
+	}
+	return coord;
+}
+
+int dupCheck(int* x) //checks for duplicate elements
+{
+	int sameNumber = 0;
+	for (int i = 0; i < 4; i++) //makes sure there are no duplicate numbers
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (x[i] == x[j])
 			{
-				cin.clear();
-				cin.ignore(numeric_limits<streamsize>::max(), '\n');
-				cout << "Oops! The number you have entered is not in range :( Maybe try something between 0 and 7?" << endl;
-				j--;
+				sameNumber++;
 			}
 		}
+	}
+	return sameNumber;
+}
+
+int guess(int* battleship)
+{
+	int guess[4], positions = 0, numbers = 0, tries = 14, sameNumber = 0;
+	for (int i = 1; i <= 13; i++) //the 13 tries the player gets
+	{
+		cout << "Try to guess the ship's 4 digits. (from 0 to 7, no repeating digits)" << endl;
+		playerInput(guess); //takes the player's guess
 		for (int ix = 0; ix < 4; ix++) //searches for correct numbers and positions
 		{
 			for (int jx = 0; jx < 4; jx++)
@@ -33,28 +55,36 @@ int guess(int* battleship)
 				}
 			}
 		}
-
-		if (positions == 4 and numbers == 4) //if the guess is fully correct
+		sameNumber = dupCheck(guess); //checks for duplicate numbers
+		if (sameNumber > 4)
 		{
-			cout << "Congratulations, you have guessed all digits and their positions correctly!" << endl;
-			tries = i;
-			i = 14;
+			cout << "You can't use the same number twice!" << endl;
+			i--;
 		}
-		else //if the guess isn't fully correct
+		else
 		{
-			cout << "----------------------------------------------------------" << endl;
-			cout << "||   Guessed    ||   ||   Player    ||   ||   Guessed   ||" << endl;
-			cout << "||   Position   ||   ||   Guesses   ||   ||   numbers   ||" << endl;
-			cout << "||   & numbers  ||   ||             ||   ||             ||" << endl;
-			cout << "----------------------------------------------------------" << endl;
-			cout << "||       " << positions << "      ||   ||   ";
-			for (int j = 0; j < 4; j++)
+			if (positions == 4 and numbers == 4) //if the guess is fully correct
 			{
-				cout << guess[j] << " ";
+				cout << "Congratulations, you have guessed all digits and their positions correctly!" << endl;
+				tries = i;
+				i = 14;
 			}
-			cout << "  ||   ||      " << numbers << "      ||" << endl;
-			cout << "----------------------------------------------------------" << endl;
-			cout << endl;
+			else //if the guess isn't fully correct
+			{
+				cout << "----------------------------------------------------------" << endl;
+				cout << "||   Guessed    ||   ||   Player    ||   ||   Guessed   ||" << endl;
+				cout << "||   Position   ||   ||   Guesses   ||   ||   numbers   ||" << endl;
+				cout << "||   & numbers  ||   ||             ||   ||             ||" << endl;
+				cout << "----------------------------------------------------------" << endl;
+				cout << "||       " << positions << "      ||   ||   ";
+				for (int j = 0; j < 4; j++)
+				{
+					cout << guess[j] << " ";
+				}
+				cout << "  ||   ||      " << numbers << "      ||" << endl;
+				cout << "----------------------------------------------------------" << endl;
+				cout << endl;
+			}
 		}
 		positions = 0; //reseting values so the code doesn't break
 		numbers = 0;
@@ -62,7 +92,7 @@ int guess(int* battleship)
 	return tries; //returning the amount of tries it took to guess
 }
 
-void Tutorial()
+void tutorial()
 {
 	char a;
 	cout << "Hello, this is our take on the Battleship game named \"Bletchley\"" << endl;
@@ -94,39 +124,20 @@ k:
 	cout << "Press ENTER to continue\n";
 }
 
-void Exit()
+void exit()
 {
 	cout << "I hope you enjoyed out little game";
 }
 
-int* bsCoord(int* battleship) //a player enters the battleship's coordinates
+int* shipCoord(int* battleship) //a player enters the battleship's coordinates
 {   
 	int sameNumber = 0;
 	do
 	{
 		sameNumber = 0;
 		cout << "Enter the 4 ship digits (must be from 0 to 7, can't use a single number more than once)" << endl;
-		for (int i = 0; i < 4; i++)
-		{
-			cin >> battleship[i];
-			if (cin.fail() or battleship[i] < 0 or battleship[i] > 7)
-			{
-				cin.clear();
-				cin.ignore(numeric_limits<streamsize>::max(), '\n');
-				cout << "Oops! One or more numbers you have entered are not in range?! Maybe try something between 0 and 7?" << endl;
-				i--;
-			}
-		}
-		for (int i = 0; i < 4; i++) //makes sure there are no duplicate numbers
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				if (battleship[i] == battleship[j])
-				{
-					sameNumber++;
-				}
-			}
-		}
+		playerInput(battleship); //used for inputting battleship coordinates
+		sameNumber = dupCheck(battleship); //checks for duplicate numbers
 		if (sameNumber > 4)
 		{
 			system("CLS");
@@ -138,7 +149,7 @@ int* bsCoord(int* battleship) //a player enters the battleship's coordinates
 
 int main()
 {
-	Tutorial();
+	tutorial();
 	cin.ignore();
 	system("CLS");
 	cout << "That's great! Let's begin." << endl;
@@ -167,7 +178,7 @@ int main()
 		{
 		case 1: //if the enemy is a player
 		{
-			bsCoord(battleship);
+			shipCoord(battleship);
 			system("CLS");
 			lastPlay = guess(battleship);
 			break;
@@ -253,5 +264,5 @@ int main()
 			}
 		}
 	} while (choice != 0);
-	Exit();
+	exit();
 }
